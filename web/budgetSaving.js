f349e4790200ae79e2a98ae4dbcf98170731eeba
Charlotte Brandhorst-Satzkorn <charlotte@catzkorn.dev>
2021-02-02 21:19:56 +0000

    Add GetMonthSaving JS
    Ability to add and view saving % for each month. If month does not have a previous % 0 is shown.

--- web/budgetSaving.js
+++ web/budgetSaving.js
@@ -1,4 +1,6 @@
-
+window.addEventListener('load', function() {
+  loadMonthSaving();
+});
 function recordSaving() {
 
   let month = document.getElementById('date-month').value;
@@ -21,6 +23,44 @@ function _postSaving(percent, date) {
   xhttp.send(data);
 }
 
+function loadMonthSaving() {
+  _getMonthSaving(_showMonthSaving);
+}
+
+function _getMonthSaving(callback) {
+
+  let month = document.getElementById('date-month').value;
+  let year = document.getElementById('date-year').value;
+
+  let xhttp = new XMLHttpRequest();
+  let path = '/api/v1/budget/saving?date=' + _formatDateForQuery(month, year);
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+      let saving = _convertToSaving(xhttp.responseText);
+      callback(saving);
+    }
+  };
+  xhttp.open("GET", path, true);
+  xhttp.send();
+
+};
+
+function _showMonthSaving(saving) {
+  document.getElementById('saving-percent').value = saving.Percent;
+}
+
+
+function _convertToSaving(response) {
+  let resSaving = JSON.parse(response);
+
+  return resSaving;
+}
+
+
 function _formatDateForJSON(month, year) {
   return year + "-" + month + "-" + "01" + "T00:00:00Z";
+}
+
+function _formatDateForQuery(month, year) {
+  return year + "-" + month + "-" + "01";
 }
